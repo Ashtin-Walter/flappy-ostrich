b3d101f9edabebd0c41e624d0e1343ab44f8f93a
Ashtin Walter <ashtinjw.dev@gmail.com>
2025-04-21 11:59:13 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   output: 'export',
-  basePath: '/flappy-ostrich',
+  basePath: '/games/original/flappy-ostrich',
 };
 
 module.exports = nextConfig;
